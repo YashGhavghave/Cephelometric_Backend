669a09385164417f4519dd2778a353c23b6e0997
YashGhavghave <yashghavghave123@gmail.com>
2025-09-29 00:03:04 +0530

    Backend for Ceph Project is completed

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Cephelometric_Backend
+
+
+#Command_to_run_server
+
+--->>  flask --app (app.py or server.py file path here) run --debug
+
+
+#Ensure to install all the packages from requirements.txt
--- Server/app.py
+++ Server/app.py
@@ -1,1 +1,29 @@
 from flask import Flask, request, jsonify
+from ultralytics import YOLO
+from io import BytesIO
+from PIL import Image
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+model = YOLO('------Model Path------')
+
+@app.route('/api/detect', methods=['POST'])
+def Handle_Image():
+    image = request.files.get('image')
+    if image is None:
+        return jsonify({'error': 'No image provided'}), 400
+    
+    results = model.predict(source = image, save=False, conf=0.5)
+    output = []
+    for r in results:
+        for box in r.boxes:
+            output.append({
+                'xyxy': box.xyxy.tolist(),
+                'confidence': float(box.conf[0]),
+                'class': int(box.cls[0])
+            })
+    return jsonify({'message': 'Image processed successfully', 'results': output}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
